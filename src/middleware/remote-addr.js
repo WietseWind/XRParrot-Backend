d9246df05cfe92f909d102c0ff523e7daba51fd3
Wietse Wind <mail@wietse.com>
2018-09-11 15:54:25 +0200

    Log content type

--- src/middleware/remote-addr.js
+++ src/middleware/remote-addr.js
@@ -7,7 +7,7 @@ module.exports = async function (expressApp) {
     req.ipTrusted = ipRange(ip, [ "", "185.40.108.0/22" ])
 
     if (typeof req.config.logRequestsToConsole !== 'undefined' && req.config.logRequestsToConsole) {
-      console.log(`>> [${req.config.mode}, trusted: ${req.ipTrusted ? 1 : 0}] Got [${req.route}] call to [${req.url}] from ${req.ip}`)
+      console.log(`>> [${req.config.mode}, trusted: ${req.ipTrusted ? 1 : 0}] Got [${req.route}] call [${req.headers['content-type']}] to [${req.url}] from ${req.ip}`)
     }
 
     next()
