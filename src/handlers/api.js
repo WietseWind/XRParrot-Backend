17cd57b104e8bbfb11384263e67b0aae42b82981
Wietse Wind <mail@wietse.com>
2018-09-11 16:29:35 +0200

    Add bunq API (beta)

--- src/handlers/api.js
+++ src/handlers/api.js
@@ -13,6 +13,26 @@ module.exports = async function (expressApp) {
     res.json({ message: 'Welcome @ XRParrot' })
   })
 
+  router.post('/api/hook', function(req, res) {
+    req.mongo.collection('hook').insertOne({
+      mode: req.config.mode,
+      trusted: req.ipTrusted,
+      ip: req.ip,
+      route: req.route,
+      url: req.url,
+      moment: new Date(),
+      headers: req.headers,
+      data: req.body
+    }, function(err, r) {
+      if (err) {
+        console.log('DB[HOOK]', err.toString())
+      } else {
+        console.log('DB[HOOK]', r.insertedCount, r.insertedId)
+      }
+    })
+    res.json({ message: 'HOOK' })
+  })
+
   router.post('/', function(req, res, next) {
     if (typeof req.body === 'object' && typeof req.body.name !== 'undefined') {
       res.json({ 
