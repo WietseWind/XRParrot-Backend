a74eabfa72ce2f021143a59c722033441953b318
Wietse Wind <mail@wietse.com>
2018-10-11 16:17:51 +0200

    Remove unn. code

--- src/handlers/api/phone.js
+++ src/handlers/api/phone.js
@@ -4,29 +4,7 @@ const messagebirdApi = require('messagebird')
 
 module.exports = (req, res) => {
   const messagebird = messagebirdApi(req.config.messagebirdKey)
-  // req.mongo.collection('hook').insertOne({
-  //   mode: req.config.mode,
-  //   trusted: req.ipTrusted,
-  //   req: {
-  //     ip: req.remoteAddress,
-  //     route: req.routeType,
-  //     url: req.url,
-  //     headers: req.headers
-  //   },
-  //   moment: new Date(),
-  //   data: req.body,
-  //   flow: {
-  //     reversal: null,
-  //     payout: null,
-  //     processed: null
-  //   }
-  // }, function(err, r) {
-  //   if (err) {
-  //     console.log('DB[HOOK]', err.toString())
-  //   } else {
-  //     console.log('DB[HOOK]', r.insertedCount, r.insertedId)
-  //   }
-  // })
+
   // messagebird.balance.read(function (err, data) {
   //   if (err) {
   //     return console.log(err)
