2e1455ad28abaa976b6bd15ae764d270abf697b3
Wietse Wind <mail@wietse.com>
2018-10-23 09:32:50 +0200

    Force prev check IBAN

--- src/handlers/api/iban-check.js
+++ src/handlers/api/iban-check.js
@@ -2,7 +2,15 @@ const ibantools = require('ibantools')
 
 module.exports = (req, res) => {
   const iban = ibantools.electronicFormatIBAN(req.body instanceof Object && typeof req.body.iban !== 'undefined' ? req.body.iban : '')
-  const valid = ibantools.isValidIBAN(iban)
+  let valid = ibantools.isValidIBAN(iban)
+
+  if (valid && (req.session.captcha || false) && ((req.session.step || -1) > 0)) {
+    req.session.step = 2
+    req.session.source = iban
+  } else {
+    console.log(`ERROR {{ INVALID @ ${req.session.id}:${__filename} }}`)
+    valid = false
+  }
 
   res.json({ message: 'IBAN Check', trusted: req.ipTrusted, valid: valid, iban: iban })
 }
