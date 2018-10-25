534685c0edbe2a93596bfd9a515976acc90ed76d
Wietse Wind <mail@wietse.com>
2018-10-25 14:47:04 +0200

    Enable hook

--- src/handlers/api/hook.js
+++ src/handlers/api/hook.js
@@ -1,29 +1,29 @@
 module.exports = (req, res) => {
-  // req.mongo.collection('hook').insertOne({
-  //   mode: req.config.mode,
-  //   trusted: req.ipTrusted,
-  //   req: {
-  //     ip: req.remoteAddress,
-  //     route: req.routeType,
-  //     url: req.url,
-  //     headers: req.headers
-  //   },
-  //   moment: new Date(),
-  //   data: req.body,
-  //   flow: {
-  //     reversal: null,
-  //     payout: null,
-  //     processed: null
-  //   }
-  // }, function(err, r) {
-  //   if (err) {
-  //     console.log('DB[HOOK]', err.toString())
-  //   } else {
-  //     console.log('DB[HOOK]', r.insertedCount, r.insertedId)
-  //   }
-  // })
+  req.mongo.collection('hook').insertOne({
+    mode: req.config.mode,
+    trusted: req.ipTrusted,
+    req: {
+      ip: req.remoteAddress,
+      route: req.routeType,
+      url: req.url,
+      headers: req.headers
+    },
+    moment: new Date(),
+    data: req.body,
+    flow: {
+      reversal: null,
+      payout: null,
+      processed: null
+    }
+  }, function(err, r) {
+    if (err) {
+      console.log('DB[HOOK]', err.toString())
+    } else {
+      console.log('DB[HOOK]', r.insertedCount, r.insertedId)
+    }
+  })
 
-  let inc
+  // let inc
   // req.mongo.collection('hook').findAndModifyOne({
   //   orders: true
   // }, {
@@ -45,5 +45,9 @@ module.exports = (req, res) => {
   //   }
   // })
 
-  res.json({ message: 'Hook received', trusted: req.ipTrusted, inc: inc })
+  res.json({ 
+    message: 'Hook received', 
+    trusted: req.ipTrusted,
+    inc: inc
+  })
 }
