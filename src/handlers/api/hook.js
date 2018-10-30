bd3a101267479f9fd8a7f0456a174bdf1ee855b4
Wietse Wind <mail@wietse.com>
2018-10-30 13:40:07 +0100

    Store only trusted hook

--- src/handlers/api/hook.js
+++ src/handlers/api/hook.js
@@ -1,53 +1,31 @@
 module.exports = (req, res) => {
-  req.mongo.collection('hook').insertOne({
-    mode: req.config.mode,
-    trusted: req.ipTrusted,
-    req: {
-      ip: req.remoteAddress,
-      route: req.routeType,
-      url: req.url,
-      headers: req.headers
-    },
-    moment: new Date(),
-    data: req.body,
-    flow: {
-      reversal: null,
-      payout: null,
-      processed: null
-    }
-  }, function(err, r) {
-    if (err) {
-      console.log('DB[HOOK]', err.toString())
-    } else {
-      console.log('DB[HOOK]', r.insertedCount, r.insertedId)
-    }
-  })
+  console.log(`<<< RECEIVED HOOK`);
+  if (req.ipTrusted) {
+    console.log(`    --> [[[ HOOK TRUSTED ]]]`);
+    req.mongo.collection('hook').insertOne({
+      mode: req.config.mode,
+      trusted: req.ipTrusted,
+      req: {
+        ip: req.remoteAddress,
+        route: req.routeType,
+        url: req.url,
+        headers: req.headers
+      },
+      moment: new Date(),
+      data: req.body,
+      flow: {
+        reversal: null,
+        payout: null,
+        processed: null
+      }
+    }, function(err, r) {
+      if (err) {
+        console.log('    DB[HOOK]', err.toString())
+      } else {
+        console.log('    DB[HOOK]', r.insertedCount, r.insertedId)
+      }
+    })
+  }
 
-  // let inc
-  // req.mongo.collection('hook').findAndModifyOne({
-  //   orders: true
-  // }, {
-  //   '$inc': {
-  //     id: 1
-  //   }
-  // }, { 
-  //   upsert: true,
-  //   projection: {
-  //     id: true
-  //   },
-  //   writeConcern: 'majority'
-  // }, function(err, r) {
-  //   inc = r
-  //   if (err) {
-  //     console.log('DB[INC]', err.toString())
-  //   } else {
-  //     console.log('DB[INC]', r.toString())
-  //   }
-  // })
-
-  res.json({ 
-    message: 'Hook received', 
-    trusted: req.ipTrusted,
-    // inc: inc
-  })
+  res.json({ message: 'OK', trusted: req.ipTrusted })
 }
