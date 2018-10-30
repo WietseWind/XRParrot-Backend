60c883e094011301dbe0838200e1e276f0505662
Wietse Wind <mail@wietse.com>
2018-10-30 10:40:30 +0100

    Skip empty local polling sessions

--- src/middleware/remote-addr.js
+++ src/middleware/remote-addr.js
@@ -4,7 +4,10 @@ module.exports = async function (expressApp) {
   expressApp.use((req, res, next) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     req.remoteAddress = ip
-    req.session.lastIp = req.remoteAddress
+    req.remoteAddressInternal = req.remoteAddress.match(/^::ffff:172\./)
+    if (!req.remoteAddressInternal) {
+      req.session.lastIp = req.remoteAddress
+    }
     req.ipTrusted = ipRange(ip, req.config.ips || "127.0.0.1")
     next()
   })
